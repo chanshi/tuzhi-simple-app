61fd77b61acbba0a62c43b814ddbb7e68acb31fc
sky_macbook <jiese@chanshi.me>
2018-03-07 16:34:34 +0800

    init

---
+++ app/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+* APP 目录
+*/
+define('APP_PATH',dirname(__DIR__).'/');
+
+/**
+ * 定义环境
+ */
+define('ENVIRONMENT','development');
+//define('ENVIRONMENT','production');
+
+
+
+/**
+ * 加载框架
+ */
+require __DIR__.'/../../vendor/tuzhi/tuzhi-simple/support/Application.php';
+require __DIR__.'/../../vendor/autoload.php';
+
+
+/**
+ * 启动框架
+ */
+\support\Application::init(
+    ( require APP_PATH.'config/config.php')
+);
+
+/**
+ * 运行框架
+ */
+App::run();
